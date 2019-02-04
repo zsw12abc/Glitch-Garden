6bd97361fcf5cad431535e2d87568bec63d67eb2
Shaowei <Shaowei.Zhang@Outlook.com>
2019-02-04 22:31:39 +1100

    Splash screen

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private int timeToWait = 3;
+    private int _currentSceneIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (_currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    private IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(_currentSceneIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
