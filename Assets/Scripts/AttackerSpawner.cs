849528d7f074d2a3dd47ebf6a9fa1e163ae804dc
Shaowei <Shaowei.Zhang@Outlook.com>
2019-02-07 21:51:27 +1100

    Add Spawner and wakatime package

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] private float minSpawnDelay = 1f;
+    [SerializeField] private float maxSpawnDelay = 5f;
+    [SerializeField] private Attacker attackerPrefab;
+    private bool spawn = true;
+
+
+    // Start is called before the first frame update
+    private IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
