d997861c111c9785938e7fa5fbd2d006c1649902
Shaowei <Shaowei.Zhang@Outlook.com>
2019-02-09 14:28:46 +1100

    Projectile shooting

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 1f;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+}
