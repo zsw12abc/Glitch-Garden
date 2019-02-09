a451a6d77e3538ec3c31b0e1ee82eff695834ab4
Shaowei <Shaowei.Zhang@Outlook.com>
2019-02-09 13:39:45 +1100

    Gun Position

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private GameObject projectile, gun;
+
+
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, gun.transform.rotation);
+    }
+}
