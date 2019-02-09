251db19316d2fd1b44821d769589479d635c3b74
Shaowei <Shaowei.Zhang@Outlook.com>
2019-02-09 15:23:12 +1100

    Shoot down enemy

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float speed = 1f;
+    [SerializeField] private float damage = 50f;
 
     private void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("I hit " + other.name);
+        var health = other.GetComponent<Health>();
+        health.DealDamage(damage);
+        Destroy(gameObject);
     }
 }
