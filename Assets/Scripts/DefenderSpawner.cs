d27a8edeefc6506458a46a36ea52c0fdb585401f
Shaowei <Shaowei.Zhang@Outlook.com>
2019-02-10 12:15:15 +1100

    Click to create defeneder

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject defender;
+
+    private void OnMouseDown()
+    {
+        SpawnDefender();
+    }
+
+    private void SpawnDefender()
+    {
+        var newDefender = Instantiate(defender, transform.position, Quaternion.identity);
+    }
+}
