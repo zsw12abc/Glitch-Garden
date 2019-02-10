4ae227577351b380d561e7231a9f9585b3fcf20d
Shaowei <Shaowei.Zhang@Outlook.com>
2019-02-10 13:49:56 +1100

    Defender Buttons UI

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach (var defenderButton in buttons)
+        {
+            defenderButton.GetComponent<SpriteRenderer>().color = new Color32(85, 85, 85, 255);
+        }
+
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
