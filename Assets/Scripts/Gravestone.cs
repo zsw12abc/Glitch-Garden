535650ecd201cb6d84c4a3240b01bd3da41274f8
Shaowei <Shaowei.Zhang@Outlook.com>
2019-02-14 21:22:55 +1100

    Add fox

---
+++ Assets/Scripts/Gravestone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravestone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var attacker = other.GetComponent<Attacker>();
+        if (attacker)
+        {
+            //TODO add some animation
+        }
+    }
+}
