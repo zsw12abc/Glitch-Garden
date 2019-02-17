69db6eb400736013a816e4dd0a9f801a177bdf3d
Shaowei <Shaowei.Zhang@Outlook.com>
2019-02-17 12:00:30 +1100

    Slider Timer

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("Our level time in SECONDS")] [SerializeField]
+    private float levelTime = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
+
+        var timerFinished = (Time.timeSinceLevelLoad >= levelTime);
+        if (timerFinished)
+        {
+            Debug.Log("Level timer expired!!");
+        }
+    }
+}
